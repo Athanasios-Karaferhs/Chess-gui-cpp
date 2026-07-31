9c38f8fe75ad09a3caa03e81ec8eadb9c6945751
Athanasios-Karaferhs <thanoskaraferis0810@gmail.com>
2026-07-31 22:42:23 +0300

    going to play with my dog:) made an idea on how to find check tho

--- Gamestate.h
+++ Gamestate.h
@@ -14,3 +14,33 @@ public:
         return turn;
     }
 };
+
+class can_it_move
+{
+public:
+    queenmove queen;
+    rookmove rook;
+    bisopmove bis;
+    horsemove horse;
+    pawnmove pawn;
+    /*  col ---->
+        row
+        |
+        |
+        \/
+    */
+    int Krow;
+    int Kcol;
+    // βρισκω που βρισκεται ο βασιλιας
+    for (int i = 0; i < 64; i++)
+    {
+        for (int j = 0; j < 64; j++)
+        {
+            if (board.at(i, j) == 5 || board.at(i, j) == -5)
+            {
+                Krow = j;
+                Kcol = i;
+            }
+        }
+    }
+};
